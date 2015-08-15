a4f931294a8badef4d1580bf1f8981365ee121a2
timsgardner <timsgardner@gmail.com>
2015-08-15 18:06:28 -0400

    dicking around

--- core.clj
+++ core.clj
@@ -3,7 +3,16 @@
         arcadia.linear
         arcadia.hydrate)
   (:require [arcadia.updater :as updr])
-  (:import [UnityEngine Transform GameObject Component PhysicMaterial Rigidbody MeshCollider BoxCollider MeshRenderer]))
+  (:import [UnityEngine Transform GameObject Component PhysicMaterial Rigidbody MeshCollider BoxCollider MeshRenderer Color]))
+
+;; (import '[UnityEngine
+;;             Color
+;;             PhysicMaterial
+;;             Transform
+;;             Rigidbody
+;;             Time]
+;;     '[UnityEditor
+;;       Selection])
 
 ;; hi there
 
@@ -38,18 +47,34 @@
     GameObject x
     Component (.gameObject x)))
 
+(defn shader-material ^Material [^String name]
+  (Material. (Shader/Find name)))
+
+(def white
+  (shader-material "Standard"))
+  
+(def black
+  (doto (shader-material "Standard")
+    (set-with! [_ color]
+      (Color. 0 0 0))))
+
 (defn tea-cupit [subcirc-n, diameter]
   (for [i (range subcirc-n)
         :let [height 1
               lp (point-pivot
-                   (v3 0.5 (+ (* height 2) 1) 0)
+                   (v3 0.5 (+ height 1) 0)
                    (v3 0)
                    (aa (* i (/ 360 subcirc-n))
                      0 1 0))]]
-    (deep-merge-mv cylinder-spec
-      {:name (str "subcirc" i)
-       :transform [{:local-position lp
-                    :local-scale [diameter height diameter]}]})))
+    (->
+      (deep-merge-mv cylinder-spec
+        {:name (str "subcirc" i)
+         :transform [{:local-position lp
+                      :local-scale [diameter height diameter]}]})
+      (assoc :mesh-renderer [{:shared-material (if (odd? i)
+                                                 white
+                                                 black)}]))))
+
 
 (defscn teacups
   (let [base-diameter 10
@@ -57,13 +82,24 @@
         base-position (v3
                         0
                         (+ 0.5 (/ (.y base-scale) 2))
-                        10)
+                        20)
         subcirc-n 3
-        subcircs (for [subc (tea-cupit subcirc-n 0.4)]
-                   (assoc subc :children
-                     (vec (tea-cupit 6 0.4))))]
+        subcircs (last
+                   (take 1
+                     (iterate
+                       (fn [kids]
+                         (for [subc (tea-cupit subcirc-n 0.59)]
+                           (-> subc
+                             (assoc :children kids)
+                             (dissoc :mesh-collider)
+                             (update-in [:transform 0 :local-position]
+                               #(v3+ % (v3 0 0 0))))))
+                       (for [subc (tea-cupit subcirc-n 0.59)]
+                         (assoc subc :children
+                           (vec (let [n 3]
+                                  (tea-cupit n (/ n (+ 1 (* 2 n)))))))))))]
     (hydrate
-      (deep-merge-mv cylinder-spec
+      (deep-merge-mv (dissoc cylinder-spec :mesh-collider)
         {:name "teacups"
          :transform [{:local-scale base-scale
                       :local-position base-position}]
@@ -77,13 +113,13 @@
 (defn teacup-driver []
   (doto (transform teacups)
     (set-with! [r localRotation]
-      (qq* r (aa 1 0 1 0))))
+      (qq* r (aa 0.2 0 1 0))))
   (let [circs-1 (map game-object (transform teacups))]
     (doseq [circ circs-1]
       (let [t (transform circ)]
         (doto t
           (set-with! [r localRotation]
-            (qq* r (aa -1 0 1 0)))))
+            (qq* r (aa -1.5 0 1 0)))))
       (doseq [trns (transform circ)]
         (set-with! trns [r localRotation]
           (qq* r (aa 1 0 1 0))))))
@@ -99,6 +135,8 @@
 
 (when-not *compile-files*
   (updr/put! :teacup-driver #'teacup-driver))
+
+(updr/toggle! :teacup-driver)
 
 
 
